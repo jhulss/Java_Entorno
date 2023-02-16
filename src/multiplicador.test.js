61da4486f7dd2e63ed6179fa77e4a382f58aece1
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-02-16 06:19:51 -0400

    Commited

--- src/multiplicador.test.js
+++ src/multiplicador.test.js
@@ -2,6 +2,7 @@ import multi from "./multiplicador";
 
 describe("Multiplocar", () => {
     it("deberia multiplicar dos numeros", () => {
-      expect(multi(3, 2)).toEqual(6);
+      //expect(multi(3, 2)).toEqual(6);
+      expect(multi(3,1)).expect(6);
     });
   });
